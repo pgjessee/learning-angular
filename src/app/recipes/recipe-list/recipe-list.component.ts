5bb8cd3c7c4205c11c41f1eef8d460e45c57f3bf
pgjessee <peter.jessee@gmail.com>
2021-05-03 18:37:31 -0500

    Basic form for the shopping-list app complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'A Test Recipe', 'https://cdn.healthyrecipes101.com/wp-content/uploads/2019/12/Healthy-Ratatouille-Recipe.jpg'),
+    new Recipe('Test Recipe', 'A Test Recipe', 'https://cdn.healthyrecipes101.com/wp-content/uploads/2019/12/Healthy-Ratatouille-Recipe.jpg')
+  ];
 
   constructor() { }
 
